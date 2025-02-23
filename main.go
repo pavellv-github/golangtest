c894da376acb47d4d560bfa48b6e916857564bdf
Pavel Lysov <pavel.lv.emails@gmail.com>
2025-02-23 21:46:19 +0300

    lesson 8

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
-	"greetings/greetings"
 )
 
 func main() {
-	var text string = "Hello world"
-	fmt.Println(text)
+	// var text string = "Hello world"
+	// fmt.Println(text)
 
-	message := greetings.Hello("Pavel")
-	fmt.Println(message)
+	// message := greetings.Hello("Pavel")
+	// fmt.Println(message)
+
+	// мой цвет
+	var favoriteColor = "white"
+	fmt.Println("Мой любимый цвет", favoriteColor)
+
+	// мой год рождения и возраст
+	birthYear, age := 1995, 29
+	fmt.Println("Я родился в", birthYear, "мне", age)
+
+	// блочное присваивание
+	var (
+		firstName = "pavel"
+		lastName  = "Lysov"
+	)
+	fmt.Println("Я", firstName, lastName)
+
+	var ageInDays int
+	ageInDays = 365 * age
+	fmt.Println("мой возраст в днях", ageInDays)
 }
