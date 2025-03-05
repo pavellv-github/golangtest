242901ee6a11abbb8d55353ed936968782117868
Pavel Lysov <pavel.lv.emails@gmail.com>
2025-03-05 21:51:16 +0300

    slices

--- main.go
+++ main.go
@@ -4,24 +4,51 @@ import (
 	"fmt"
 )
 
+var slice = make([]int, 20)
+
+func iterate(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		item := slice[i]
+		fmt.Println("asd", item)
+	}
+}
+
 func main() {
-	myArray := [3]int{1, 2, 3}
-	fmt.Println(myArray[2])
+	mySlice := []int{1, 2, 3, 4, 5, 6, 7}
 
-	for i := 0; i < len(myArray); i++ {
-		item := myArray[i]
-		fmt.Println("test a", item)
+	slice1 := mySlice[:]
+	slice2 := mySlice[1:3]
+	slice3 := mySlice[4:6]
+	slice4 := mySlice[:6]
+
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+	fmt.Println(slice3)
+	fmt.Println(slice4)
+
+	mySlice = append(mySlice, 8, 9, 10, 11)
+	fmt.Println(mySlice)
+
+	mySlice2 := []int{12, 13, 14, 15}
+	mySlice3 := append(mySlice, mySlice2...)
+	fmt.Println(mySlice3)
+
+	small := []int{1}
+	big := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	iterate(small)
+	iterate(big)
+
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		{"_", "_", "_"},
+		{"_", "_", "_"},
 	}
 
-	for i := 0; i < 10; i++ {
-		item := myArray[i]
-		fmt.Println("test b", item)
-	}
+	board[0][0] = "X"
+	board[2][2] = "0"
+	board[1][2] = "X"
+	board[1][0] = "0"
+	board[0][2] = "X"
 
-	myArray2 := [...]int{1, 2, 3, 4, 5, 6, 7}
-
-	for i := 0; i < 10; i++ {
-		item := myArray2[i]
-		fmt.Println("test c", item)
-	}
+	fmt.Println(board)
 }
