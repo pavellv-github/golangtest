e341b1caca700e2652e3e025eb73dca857a3652a
Pavel Lysov <pavel.lv.emails@gmail.com>
2025-03-12 20:29:41 +0300

    maps

--- main.go
+++ main.go
@@ -4,51 +4,26 @@ import (
 	"fmt"
 )
 
-var slice = make([]int, 20)
-
-func iterate(slice []int) {
-	for i := 0; i < len(slice); i++ {
-		item := slice[i]
-		fmt.Println("asd", item)
-	}
-}
-
 func main() {
-	mySlice := []int{1, 2, 3, 4, 5, 6, 7}
+	//  myMap := make(map[string]int);
 
-	slice1 := mySlice[:]
-	slice2 := mySlice[1:3]
-	slice3 := mySlice[4:6]
-	slice4 := mySlice[:6]
-
-	fmt.Println(slice1)
-	fmt.Println(slice2)
-	fmt.Println(slice3)
-	fmt.Println(slice4)
-
-	mySlice = append(mySlice, 8, 9, 10, 11)
-	fmt.Println(mySlice)
-
-	mySlice2 := []int{12, 13, 14, 15}
-	mySlice3 := append(mySlice, mySlice2...)
-	fmt.Println(mySlice3)
-
-	small := []int{1}
-	big := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	iterate(small)
-	iterate(big)
-
-	board := [][]string{
-		[]string{"_", "_", "_"},
-		{"_", "_", "_"},
-		{"_", "_", "_"},
+	myMap := map[string]int{
+		"item1": 1,
+		"item2": 2,
+		"item3": 3,
 	}
 
-	board[0][0] = "X"
-	board[2][2] = "0"
-	board[1][2] = "X"
-	board[1][0] = "0"
-	board[0][2] = "X"
+	fmt.Println(myMap["item1"])
 
-	fmt.Println(board)
+	delete(myMap, "item2")
+
+	fmt.Println(myMap["item2"])
+
+	myMap["item2"] = 222
+
+	fmt.Println(myMap["item2"])
+
+	for key, value := range myMap {
+		fmt.Println(key, value)
+	}
 }
